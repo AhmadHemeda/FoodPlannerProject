e45ea4b7cf2deb1f0ff870e03daa5c9f0e7c9690
Asalah3 <88517428+Asalah3@users.noreply.github.com>
2023-01-12 21:32:25 +0200

    FirebaseAuthentication

--- app/src/main/java/com/example/foodplanner/view/MainActivity.java
+++ app/src/main/java/com/example/foodplanner/view/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.foodplanner.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.foodplanner.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
